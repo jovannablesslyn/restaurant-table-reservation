e8e9dc529345d8e2bbfd62f24feca406e955f2e7
JOVANNA BLESSLYN URK23CS1052 <jovannablesslyn@karunya.edu.in>
2025-11-07 00:10:46 +0530

    Initial commit with corrected server and static frontend

--- server.js
+++ server.js
@@ -2,17 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public")); // to serve index.html
+
+// Serve static files (index.html, style.css, etc.) from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Import routes
 const reservationRoutes = require("./routes/reservations");
 app.use("/api/reservations", reservationRoutes);
+
+// Catch-all: serve index.html for all non-API, non-file requests (for SPA support)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/restaurant_reservations";
